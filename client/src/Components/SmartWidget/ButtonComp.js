97ba3bfaee36d78168024bbdad78a3016cb35c1d
Moos <mostafa.khaldi@outlook.com>
2024-08-16 18:09:36 +0800

    bug fix in smart widgets

--- client/src/Components/SmartWidget/ButtonComp.js
+++ client/src/Components/SmartWidget/ButtonComp.js
@@ -70,7 +70,7 @@ export default function ButtonComp({
   useEffect(() => {
     setButtonColor(getButtonColor());
     setButtonUrl(getUrl());
-  }, [type]);
+  }, [type, backgroundColor, textColor]);
 
   if (buttonUrl !== false)
     return (
